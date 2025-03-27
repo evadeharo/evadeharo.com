94b0731bfc50f369d6c629641ea178c333038bbe
Eva De Haro <evadeharomunoz@gmail.com>
2025-03-27 18:47:47 +0100

    fix: animation init fix

--- src/components/BgCanvas.tsx
+++ src/components/BgCanvas.tsx
@@ -1,21 +1,39 @@
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { Html } from "@react-three/drei";
-import { useRef } from "react";
-
-import * as THREE from "three"
+import { useRef, useState } from "react";
+import * as THREE from "three";
 
 export function BgCanvas() {
   const groupRef = useRef<THREE.Group | null>(null);
+  const [animating, setAnimating] = useState(true);
+  const { viewport } = useThree();
+
+  const top = viewport.height / 2;
+  const bottom = -viewport.height / 2;
+  const threshold = 0.4;
+
+  const [position, setPosition] = useState(top)
 
   useFrame((_, delta) => {
-    if (!groupRef.current) return;
+    if (!groupRef.current || !animating) return
 
-    groupRef.current.position.y = 
-      THREE.MathUtils.lerp(groupRef.current.position.y, -3, 0.05 * delta);
+    const currentY = groupRef.current.position.y;
+    const distanceToBottom = Math.abs(currentY - bottom);
+
+    if (distanceToBottom < threshold) {
+      setPosition(currentY)
+      setAnimating(false);
+      return;
+    }
+
+    groupRef.current.position.y = Math.max(
+      THREE.MathUtils.lerp(currentY, bottom, 0.30 * delta),
+      bottom
+    );
   });
 
   return (
-    <group ref={groupRef} position={[0, 3, 0]}> 
+    <group ref={groupRef} position={[0, position, 0]}>
       <Html
         occlude
         as="div"
