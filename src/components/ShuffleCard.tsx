1e8576c7d211853c3cc21fad4442a53fb452ed93
Eva De Haro <evadeharomunoz@gmail.com>
2025-06-03 22:03:38 +0200

    feat: shuffle action

--- src/components/ShuffleCard.tsx
+++ src/components/ShuffleCard.tsx
@@ -5,30 +5,38 @@ import { Group, Vector3 } from "three";
 import { ShuffleCardType } from "../mock/data";
 
 type Props = Omit<ShuffleCardType, "id"> & {
-  index: number;
+  index?: number;
 };
 
 export default function ShuffleCard({ title, subtitle, index }: Props) {
   const groupRef = useRef<Group>(null);
   const [targetPosition, setTargetPosition] = useState<Vector3 | null>(null);
 
-  const initialPosition = useMemo(
-    () => new Vector3(0, 0, index * 0.4),
-    [index]
-  );
+  const initialPosition = useMemo(() => {
+    const stepY = -0.4;
+    const stepX = 1;
+    const i = index ?? 0;
+
+    const y = stepY * i + 1.8;
+    const x = stepX * i - 2.5;
+
+    const z = (1.8 - y) * -1;
+
+    return new Vector3(x, y, z);
+  }, [index]);
 
   function generateEscapePosition(): Vector3 {
     const direction = new Vector3(
       Math.random() * 2 - 1,
       Math.random() * 2 - 1,
-      Math.random() * 1 + 1
+      0
     );
     return initialPosition.clone().add(direction.multiplyScalar(1.5));
   }
 
   useFrame(() => {
     if (!groupRef.current || !targetPosition) return;
-    groupRef.current.position.lerp(targetPosition, 0.05);
+    groupRef.current.position.lerp(targetPosition, 0.35);
 
     if (groupRef.current.position.distanceTo(targetPosition) < 0.01) {
       groupRef.current.position.copy(targetPosition);
@@ -37,16 +45,20 @@ export default function ShuffleCard({ title, subtitle, index }: Props) {
   });
 
   return (
-    <group ref={groupRef} position={initialPosition.toArray()}>
-      <Html occlude>
+    <group
+      ref={groupRef}
+      position={initialPosition.toArray()}
+      renderOrder={index}
+    >
+      <Html occlude zIndexRange={[100, 0]}>
         <div
-          className="bg-gray-secondary border border-px border-black w-[28rem] h-max pointer-events-auto"
+          className="bg-gray-secondary border border-px border-black w-[25rem] h-max pointer-events-auto"
           onMouseEnter={() => setTargetPosition(generateEscapePosition())}
           onClick={() => setTargetPosition(generateEscapePosition())}
         >
           <h2 className="p-4 text-[1.75rem] font-bold">{title}</h2>
           <div className="w-full h-px bg-black" />
-          <h3 className="p-4 text-[1.25rem]">{subtitle}</h3>
+          <h3 className="p-4 text-[1.15rem]">{subtitle}</h3>
         </div>
       </Html>
     </group>
--- src/mock/data.ts
+++ src/mock/data.ts
@@ -18,40 +18,41 @@ export const mockedData: MockedDataTypes = {
     contact: "evadeharomunoz@gmail.com",
     contactHref: "mailto:evadeharomunoz@gmail.com",
     items: [
+    
         {
             id: "card-01",
-            title: "I don't know how to code",
-            subtitle: "But I've made this cool website",
-        },
-        {
-            id: "card-02",
-            title: "I've forgotten the design baselines",
-            subtitle: "But since I graduated, I have been reading and learning about composition, color and new trends and tendencies",
-        },
-        {
-            id: "card-03",
             title: "My creativity is dead",
             subtitle: "But I'm non-stop trying to learn how to express myself the way I want to, without dying in the process",
         },
         {
-            id: "card-04",
+            id: "card-02",
             title: "My free time == My dog",
             subtitle: "If you want to find me after my work hours, it's very possible that I'm walking with my dog",
         },
         {
-            id: "card-05",
+            id: "card-03",
             title: "I sky, I swim, I run",
             subtitle: "I decompress my anxiety through sports, I can be considered a 'gymbro'",
         },
         {
-            id: "card-06",
+            id: "card-04",
             title: "The more movement, the better",
             subtitle: "'Less is more' is not my way to go",
         },
         {
-            id: "card-07",
+            id: "card-05",
             title: "Team goes first",
             subtitle: "I have a necessity to constantly meet up with the people who hang around me to know them better",
+        },
+        {
+            id: "card-06",
+            title: "I've forgotten the design baselines",
+            subtitle: "But since I graduated, I have been reading and learning about composition, color and new trends and tendencies",
+        },
+        {
+            id: "card-07",
+            title: "I don't know how to code",
+            subtitle: "But I've made this cool website",
         },
     ]
 }
